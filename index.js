3ff2d245538edc79e5b7218fc7da62efb317a623
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-04 00:13:18 +0600

    categories added

--- index.js
+++ index.js
@@ -17,10 +17,30 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // database and collection
+const categoriesCollection = client.db('inventory').collection('categories');
 const productsCollection = client.db('inventory').collection('products');
 
 async function run() {
     try {
+
+        // get categories from database
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories)
+        })
+        // add categories to database
+        app.post('/categories', async (req, res) => {
+            const category = req.body;
+            const query = { name: category.name }
+            const previousCategory = await categoriesCollection.findOne(query);
+            if (previousCategory) {
+                return res.send({ message: 'Category already exists' })
+            }
+            const result = await categoriesCollection.insertOne(category);
+            res.send(result);
+        })
+
         // get product from database
         app.get('/products', async (req, res) => {
             const query = {};
@@ -47,6 +67,12 @@ async function run() {
             };
             const updateResult = await productsCollection.updateOne(filter, updatedDoc)
             res.send(updateResult)
+        })
+        // delete a product from database
+        app.delete('/products/:id', async (req, res) => {
+            const filter = { _id: ObjectId(req.params.id) };
+            const result = await productsCollection.deleteOne(filter);
+            res.send(result)
         })
     } catch (error) {
 
