42186ab27ebf23502ac69bac14b31838d543634d
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-03 20:33:20 +0600

    product added successfully

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // mongodb connection
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhwsqpg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -22,11 +22,31 @@ const productsCollection = client.db('inventory').collection('products');
 async function run() {
     try {
         // get product from database
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        })
+        app.get('/products/:id', async (req, res) => {
+            const query = { _id: ObjectId(req.params.id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product)
+        })
         // add product to database
         app.post('/products', async (req, res) => {
             const product = req.body;
             const result = await productsCollection.insertOne(product);
             res.send(result)
+        })
+        // update product in database
+        app.patch('/products/:id', async (req, res) => {
+            const filter = { _id: ObjectId(req.params.id) };
+            console.log(req.body)
+            const updatedDoc = {
+                $set: req.body
+            };
+            const updateResult = await productsCollection.updateOne(filter, updatedDoc)
+            res.send(updateResult)
         })
     } catch (error) {
 
